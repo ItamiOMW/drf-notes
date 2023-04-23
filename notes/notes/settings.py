490f74e2a499489684840069ac2ff2708ab3ba8f
Вадим <itamiomw@gmail.com>
2023-04-23 14:02:25 +0300

    Note model created

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'notes',
+    'notes_app.apps.NotesAppConfig',
 ]
 
 MIDDLEWARE = [
